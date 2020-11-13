62f391bbc3a7855eab219f8621fcfeebf93bde85
josecxstillo <josecxstillo@users.noreply.github.com>
2020-11-13 13:49:58 -0600

    Update

--- src/controladores/VistaHistorialController.java
+++ src/controladores/VistaHistorialController.java
@@ -7,7 +7,13 @@ package controladores;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
 /**
  * FXML Controller class
@@ -16,6 +22,45 @@ import javafx.fxml.Initializable;
  */
 public class VistaHistorialController implements Initializable {
 
+    @FXML
+    private TextField txtnoHistorial;
+    @FXML
+    private TextField txtnoConsulta;
+    @FXML
+    private DatePicker txtfecha;
+    @FXML
+    private TextField txtPaciente;
+    @FXML
+    private TextField txtedad;
+    @FXML
+    private TextField txtpeso;
+    @FXML
+    private TextField txtaltura;
+    @FXML
+    private TextField txttipoSangre;
+    @FXML
+    private TextField txtTemperatura;
+    @FXML
+    private TextField txtpresion;
+    @FXML
+    private TextField txtpulso;
+    @FXML
+    private TextField txtrespiracion;
+    @FXML
+    private TextField txtmedico;
+    @FXML
+    private TextArea txtananmesis;
+    @FXML
+    private TextArea txttratamiento;
+    @FXML
+    private Button btnbConsulta;
+    @FXML
+    private Button btnGuardar;
+    @FXML
+    private Button btnCancelar;
+    @FXML
+    private Button btnEliminar;
+
     /**
      * Initializes the controller class.
      */
@@ -23,5 +68,21 @@ public class VistaHistorialController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    @FXML
+    private void buscarConsulta(ActionEvent event) {
+    }
+
+    @FXML
+    private void agregarConsulta(ActionEvent event) {
+    }
+
+    @FXML
+    private void cancelar(ActionEvent event) {
+    }
+
+    @FXML
+    private void eliminarConsulta(ActionEvent event) {
+    }
     
 }
