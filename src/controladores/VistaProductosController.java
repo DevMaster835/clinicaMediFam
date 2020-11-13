db7ab89339c64d008d6c56f08ba2a5642f9433b0
josecxstillo <josecxstillo@users.noreply.github.com>
2020-11-13 03:18:21 -0600

    Update

--- src/controladores/VistaProductosController.java
+++ src/controladores/VistaProductosController.java
@@ -5,9 +5,20 @@
  */
 package controladores;
 
+import Conexion.conexion;
+import com.mysql.jdbc.Connection;
 import java.net.URL;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javax.swing.JOptionPane;
 
 /**
  * FXML Controller class
@@ -15,6 +26,32 @@ import javafx.fxml.Initializable;
  * @author José
  */
 public class VistaProductosController implements Initializable {
+    
+    conexion con= new conexion();
+    Connection cone= con.openConnection();
+    
+    PreparedStatement pps;
+
+    @FXML
+    private TextField txtfechaVen;
+    @FXML
+    private Button btnEliminar;
+    @FXML
+    private Button btnCancelar;
+    @FXML
+    private Button btnGuardar;
+    @FXML
+    private TextField txtcodigoProd;
+    @FXML
+    private TextField txtnombreProd;
+    @FXML
+    private TextField txtPrecio;
+    @FXML
+    private TextField txtExistencia;
+    @FXML
+    private TextField txtconNeto;
+    @FXML
+    private TextField txtFabricante;
 
     /**
      * Initializes the controller class.
@@ -22,6 +59,37 @@ public class VistaProductosController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
+    }  
+    
+        @FXML
+    private void agregarProductos(ActionEvent event) {
+        
+        try{
+            pps=cone.prepareStatement("INSERT INTO productos(idProducto,nombre,idPrecioHis,fechaVencimiento,stock,contenidoNeto,idFabricante VALUES(?,?,?,?,?,?,?)");
+            pps.setString(1, txtcodigoProd.getText());
+            pps.setString(2, txtnombreProd.getText());
+            pps.setString(3, txtPrecio.getText());
+            pps.setString(4, txtfechaVen.getText());
+            pps.setString(5, txtExistencia.getText());
+            pps.setString(6, txtconNeto.getText());
+            pps.setString(7, txtFabricante.getText());
+            pps.executeUpdate();
+            
+            JOptionPane.showMessageDialog(null, "Se ha registrado los datos del producto", "Datos guardados", JOptionPane.PLAIN_MESSAGE);
+        }catch(SQLException ex){
+            Logger.getLogger(VistaPacientesController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+    }
+
+    @FXML
+    private void eliminarProductos(ActionEvent event) {
+    }
+
+    @FXML
+    private void cancelarProductos(ActionEvent event) {
+    }
+
+
     
 }
